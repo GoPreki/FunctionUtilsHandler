8d7243113f32f67308ca6c43e92a35a29561dd34
Mariana Rodríguez <m.rodriguez21@uniandes.edu.co>
2021-12-31 10:35:29 -0500

    feat[exceptions]: Modify PrekiException to allow custom logging
    For more flexibility for logging exceptions

--- preki_funcutils/exceptions.py
+++ preki_funcutils/exceptions.py
@@ -1,11 +1,27 @@
+from typing import Optional
 from . import status
+from .logger import LogLevel
 
 
 class PrekiException(Exception):
 
-    def __init__(self, message, error_code=None, status_code=status.HTTP_400_BAD_REQUEST, data=None):
+    def __init__(
+        self,
+        message: str,
+        error_code: str = None,
+        status_code: int = status.HTTP_400_BAD_REQUEST,
+        data: Optional[dict] = None,
+        log_event: Optional[str] = None,
+        log_data: dict = {},
+        log_level: LogLevel = LogLevel.ERROR,
+        force_error: bool = False,
+    ):
         super().__init__(message)
         self.message = message
         self.status_code = status_code
         self.error_code = error_code
         self.data = data
+        self.force_error = force_error
+        self.log_event = log_event
+        self.log_data = log_data
+        self.log_level = log_level
--- preki_funcutils/functions.py
+++ preki_funcutils/functions.py
@@ -104,7 +104,18 @@ def lambda_response(func):
             response = func(event, context, *args, **kwargs)
             return _make_response(origin=origin, stage=stage, body=response)
         except exceptions.PrekiException as e:
-            log(level=LogLevel.WARNING, message=e.message, args=e.data)
+            if e.log_event:
+                log(level=e.log_level,
+                    event=e.log_event,
+                    message=e.message,
+                    args={
+                        **(e.log_data or e.data or {}),
+                        'status_code': e.status_code,
+                    })
+
+            if e.force_error:
+                raise e
+
             return _make_error(origin=origin,
                                stage=stage,
                                type=type(e).__name__,
